1f6af10b570fd4de3c91c693471dba5f950fd4f9
zeynalabidinunlu <zeynalunlu@gmail.com>
2026-02-01 00:17:48 +0300

    created the ErrorDto class,created the GlobalExceptionHandler class

---
+++ src/main/java/com/zeynalabidinunlu/tickets/domain/dtos/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.zeynalabidinunlu.tickets.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+
+	private String error;
+}
